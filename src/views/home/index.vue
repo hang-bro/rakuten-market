5f813414da77162824d7c3e14b13d5d2cbf07518
huangyuhang <772269729@qq.com>
2024-06-20 13:08:24 +0800

    img

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,13 +4,16 @@
     <iframe class="w-full h-[550px]" src="./header.html" frameborder="0"></iframe>
     <!-- <Recommend /> -->
     <!-- <Content /> -->
-    <div class="bg-[url('https://image.rakuten.co.jp/arkstore/cabinet/imgrc0116673270.jpg')] bg-no-repeat " style="background-size: 100%;">
+    <div
+      class="bg-[url('https://image.rakuten.co.jp/arkstore/cabinet/imgrc0116673270.jpg')] bg-no-repeat"
+      style="background-size: 100%"
+    >
       <div class="flex w-[1200px] m-auto pt-[300px]">
         <iframe class="w-[205px] mr-10" src="./menu.html" frameborder="0"></iframe>
         <iframe class="w-full h-[1500px]" src="./content.html" frameborder="0"></iframe>
       </div>
     </div>
-    <Footer />
+    <iframe class="w-full h-[500px]" src="./footer.html" frameborder="0"></iframe>
   </main>
 </template>
 <script lang="ts" setup>
